60f444d20d99ee9d439fb5762d49a64bf6009e57
AugustoSavi <guto_savi@outlook.com>
2021-01-30 01:45:27 -0300

    Final-Aula 04-Aula 05

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,8 @@ import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-
+import { motion } from 'framer-motion';
+import Link from '../src/components/Link';
 
 const Title = styled.h1`
   font-size: 50px;
@@ -63,16 +64,44 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/AugustoSavi"/>
     </QuizBackground>
   );
 }
